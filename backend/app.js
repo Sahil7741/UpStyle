c1b4cc67b78816b444448a3d02988e50fa11c2cf
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2024-01-10 22:28:06 +0530

    Update

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ const startServer = async () => {
 startServer();
 
 // Middleware for handling CORS
-app.use(cors({ origin: "https://sahil7741.github.io", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND , credentials: true }));
 
 // Middleware for parsing cookies
 app.use(cookieParser());
